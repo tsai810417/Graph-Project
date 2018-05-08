d1b3e8783b35af00d05e4bade8a2e699041aa48e
tsai810417 <tsai_cheng@yahoo.com>
2018-05-08 11:21:13 -0700

    create method to sort vertices

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -1,7 +1,33 @@
 require_relative 'graph'
-
+require 'byebug'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  top = []
 
+  # sorted = vertices.select{ |v| v.in_edges.empty? }
+
+  vertices.each do | v |
+    if v.in_edges.empty?
+      top.push(v)
+    end
+  end
+
+
+  until top.empty?
+    current = top.pop
+    sorted << current
+    arr = current.out_edges.dup
+    arr.each do | e |
+      v = e.to_vertex
+      e.destroy!
+      if v.in_edges.empty?
+        top.push(v)
+      end
+
+    end
+  end
+  
+  sorted.length == vertices.length ? sorted : []
 end
